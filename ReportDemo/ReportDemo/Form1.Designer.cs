ada9a48558a7b08c19ebfafe375c049dc4134ae2
Stephen <soutieslak@gmail.com>
2017-07-13 09:00:36 +0200

    Added the demo

---
+++ ReportDemo/ReportDemo/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ReportDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(414, 591);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(454, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 87);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Report Module to this tabControl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 615);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ReportDemo/ReportDemo/Module_Report/Reporting_Word.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Novacode;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Module_Report
+{
+    public class Reporting_Word
+    {
+
+        public struct Bookmarks
+        {
+            public string Name;
+            public string FilterTag;
+            public string Text;
+            public Bitmap Figure;
+            public bool Mapped;
+        }
+
+
+
+        public Reporting_Word()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a Novacode Picture from a local image file such as .PNP, .JPG etc
+        /// </summary>
+        /// <param name="document">Referce to the global document</param>
+        /// <param name="Directory">File directory of the local image file</param>
+        /// <param name="Height">Height of the picture</param>
+        /// <param name="Width">Width of the picture</param>
+        /// <returns>Returns a picture of type Novacode.Picture</returns>
+        public Picture CreatePicture(ref DocX document, string Directory, int Height, int Width)
+        {
+            Novacode.Image image = document.AddImage(Directory);
+            // Create a picture (A custom view of an Image).
+            Picture picture = image.CreatePicture();
+
+            picture.Height = Height;
+            picture.Width = Width;
+
+            return picture;
+        }
+
+        /// <summary>
+        /// Creates a Novacode Picture from a Bitmap
+        /// </summary>
+        /// <param name="document">Referce to the document</param>
+        /// <param name="bmp">Bitmap to be converted</param>
+        /// <param name="Height">Height of the picture</param>
+        /// <param name="Width">Width of the picture</param>
+        /// <returns>Returns a picture of type Novacode.Picture</returns>
+        public Picture CreatePicture(ref DocX document, Bitmap bmp, int Height, int Width)
+        {
+            System.Drawing.Image image = (System.Drawing.Image)bmp;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);// Save your picture in a memory stream.
+                //image.Save(ms, bmp.RawFormat);  
+                ms.Seek(0, SeekOrigin.Begin);
+
+                Novacode.Image NovaImage = document.AddImage(ms); // Create image.
+
+                Picture picture = NovaImage.CreatePicture();     // Create picture.
+
+                picture.Height = Height;
+                picture.Width = Width;
+
+                return picture;
+            }
+        }
+
+        /// <summary>
+        /// Saves the document as a file on your computer
+        /// </summary>
+        /// <param name="document">The document you want to write to a file</param>
+        /// <param name="FilePath">The location to save the file (C:\Document.docx) </param>
+        public void SaveDoc(DocX document, string FilePath)
+        {
+            document.SaveAs(FilePath);
+        }
+
+        /// <summary>
+        /// Loads an existing word (.docx) document
+        /// </summary>
+        /// <returns>Returns the opened document</returns>
+        public DocX Load_Template()
+        {
+            OpenFileDialog LoadDoc = new OpenFileDialog();
+            LoadDoc.Title = "Load document template";
+            LoadDoc.Filter = "Word document |*.docx|All Supported Files |*.docx|All files|*.*";
+            LoadDoc.FilterIndex = 1;    //By default the .docx file is selected
+            LoadDoc.Multiselect = false;
+
+            if (LoadDoc.ShowDialog() == DialogResult.OK)
+            {
+                DocX document = DocX.Load(LoadDoc.FileName);
+                return document;
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// Loads an existing word (.docx) document
+        /// </summary>
+        /// <param name="FilePath">The file path of the document e.g. C:\document.docx</param>
+        /// <returns>Returns the opened document</returns>
+        public DocX Load_Template(string FilePath)
+        {
+            DocX document = DocX.Load(FilePath);
+            return document;
+        }
+
+        /// <summary>
+        /// Gets a list of all the 
+        /// </summary>
+        /// <param name="document">Referce to the document</param>
+        /// <returns>Returns a list of type string containing the names of all available bookmarks</returns>
+        public List<Bookmarks> List_Bookmarks(DocX document)
+        {
+            List<Bookmarks> BookmarkList = new List<Bookmarks>();
+            Bookmarks NewBookmark;
+            foreach (Bookmark bookmark in document.Bookmarks)
+            {
+                //Perform a check to make sure it only adds applicable bookmarks (there are lots of random ones such as _Toc34827482 which we don't want)
+                if (bookmark.Name.Contains("bmk"))
+                {
+                NewBookmark.Name = bookmark.Name;
+                NewBookmark.Text = "";
+                NewBookmark.Figure = null;
+                NewBookmark.FilterTag = "";
+                NewBookmark.Mapped = false;
+                BookmarkList.Add(NewBookmark);
+                Console.WriteLine("\t\tFound bookmark {0}", bookmark.Name);
+                }
+            }
+            return BookmarkList;
+        }
+
+        /// <summary>
+        /// Inserts a picture by the selected bookmark
+        /// </summary>
+        /// <param name="document">Referce to the document</param>
+        /// <param name="bookmark">The exact name of the bookmark you want to change</param>
+        /// <param name="picture">The picture you want to insert</param>
+        /// <returns>Returns the modified document</returns>
+        public DocX Bookmark_Change(DocX document, string bookmark, Picture picture)
+        {
+            document.Bookmarks[bookmark].Paragraph.AppendLine("").AppendPicture(picture);
+            return document;
+        }
+
+        /// <summary>
+        /// Inserts text by the selected bookmark
+        /// </summary>
+        /// <param name="document">Referce to the document</param>
+        /// <param name="bookmark">The exact name of the bookmark you want to change</param>
+        /// <param name="text">The new text of the selected bookmark</param>
+        /// <returns>Returns the modified document</returns>
+        public DocX Bookmark_Change(DocX document, string bookmark, string text)
+        {
+            document.Bookmarks[bookmark].SetText(text);
+            return document;
+        }
+
+    }
+}
